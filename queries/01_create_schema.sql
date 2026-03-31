10af7c7bcd487e93f15afbf31e56200bf4858b7c
MichaelM <158190116+Michael-Mattsson@users.noreply.github.com>
2026-04-01 01:17:45 +0300

    Create schema for results dataset
    This schema is intended for storing results data.

--- queries/01_create_schema.sql
+++ queries/01_create_schema.sql
@@ -1,1 +1,6 @@
+-- ============================================
+-- Create Results Dataset (Run Once)
+-- ============================================
 
+CREATE SCHEMA `project-ebfa42f4-7d2c-45b2-89a.Results`
+OPTIONS(location = "US");
